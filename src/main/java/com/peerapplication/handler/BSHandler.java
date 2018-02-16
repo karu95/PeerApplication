f8c4c271a921cd782c2655bf0a8482272a3264ec
mkarunanayake <mevan.15@cse.mrt.ac.lk>
2018-02-16 22:03:51 +0530

    peerApplicationUpdated

--- src/main/java/com/peerapplication/handler/BSHandler.java
+++ src/main/java/com/peerapplication/handler/BSHandler.java
@@ -1,4 +1,25 @@
 package com.peerapplication.handler;
 
+import com.peerapplication.message.LoginMessage;
+import com.peerapplication.message.LogoutMessage;
+import com.peerapplication.message.PasswordChangeMessage;
+import com.peerapplication.message.RegisterMessage;
+
 public class BSHandler {
+    public void login(LoginMessage loginMessage){
+
+    }
+
+    public void register(RegisterMessage registerMessage){
+
+    }
+
+    public void logout(LogoutMessage logoutMessage){
+
+    }
+
+    public void changePassword(PasswordChangeMessage pwChangeMessage){
+
+    }
+
 }
